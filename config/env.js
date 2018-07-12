1fff32eb875b33bd72c5f8148d2f60c46f5c1368
Aas Suhendar <aas.suhendar@gmail.com>
2018-07-12 10:04:04 +0700

    Update env.js

--- config/env.js
+++ config/env.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv').config()
 
 module.exports = {
   database_dev: process.env.MONGO_URL_DEV,
-  database_test: process.env.MONGO_URL_TEST,
+  //database_test: process.env.MONGO_URL_TEST,
+  database_test: "mongodb://playcourt:playcourtTelkom@mongodb-for-testing-api.apps.playcourt.id:30308/test-db",
   database_prod: process.env.MONGO_URL_PROD
 }
