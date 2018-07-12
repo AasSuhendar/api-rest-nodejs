8cee4e03c052414c7487a1b95308c2423f1e53f6
Aas Suhendar <aas.suhendar@gmail.com>
2018-07-12 11:50:17 +0700

    update env mongo connection

--- config/env.js
+++ config/env.js
@@ -3,6 +3,6 @@ const dotenv = require('dotenv').config()
 module.exports = {
   database_dev: process.env.MONGO_URL_DEV,
   //database_test: process.env.MONGO_URL_TEST,
-  database_test: "mongodb://playcourt:playcourtTelkom@mongodb-for-testing-api.apps.playcourt.id:30308/test-db",
+  database_test: "mongodb://playcourt:playcourtTelkom@mongodb-todos-demoplaycourt.apps.playcourt.id:30845/db-todos",
   database_prod: process.env.MONGO_URL_PROD
 }
