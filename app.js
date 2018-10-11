02c0b39094f055a923d6868bba78af91e70fcaaa
Aas Suhendar <aas.suhendar@gmail.com>
2018-10-11 23:13:24 +0700

    add swagger docs

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
-
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 var mongooseConf = require('./helper/mongooseConf')
 var loggerFile = require('./config/logger')
 var index = require('./routes/index')
 var todos = require('./routes/todos')
+
+// const swaggerDocument = require('./swagger/swagger.json');
+const swaggerDocument = YAML.load('./swagger/swagger.yaml');
 
 var app = express()
 var conDB = new mongooseConf(app)
@@ -23,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', index)
 app.use('/api/todo-list', todos)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function (req, res) {
